39defe5b83f19896ad03317b7893acbd3d4949be
hreinn91 <hreinnj@localhost.localdomain>
2020-01-06 16:29:24 +0100

    Implementing Mongo to PostgreSQL migration

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 from data_aggregator import data_aggregation
+from data_aggregator import data_aggregation_to_pg
 
 mongo_db_name = 'testdb'
+pg_db_name = 'test_db'
+
 original_collection_name = 'Task'
 new_collection_name = 'TaskCount'
 
-data_aggregation(mongo_db_name, original_collection_name, new_collection_name)
+# data_aggregation(mongo_db_name, original_collection_name, new_collection_name)
+
+data_aggregation_to_pg(mongo_db_name, new_collection_name, pg_db_name)
 
 
--- misc.py
+++ misc.py
@@ -1,57 +1,8 @@
-from pymongo import MongoClient
-import datetime
-import pprint
-from bson.son import SON
+import sql
 
-test_db_name = 'testdb'
-collection_name = 'Task'
+some_table = sql.Table('some_table')
+select = some_table.select()
 
-id_key = 'taskId'
-unit_key = 'unit'
-time_key = 'datetime'
+x = tuple(select)
 
-numb_mocked_docs = 1000
-
-client = MongoClient()
-db = client[test_db_name]
-col = db[collection_name]
-
-proj = {"$project": {
-    "y": {"$year": "$" + time_key},
-    "m": {"$month": "$" + time_key},
-    "d": {"$dayOfMonth": "$" + time_key},
-    "h": {"$hour": "$" + time_key},
-    "u": "$" + unit_key}
-}
-
-group = {"$group": {
-    "_id": {"year": "$y", "month": "$m", "day": "$d", "hour": "$h", unit_key: "$u"},
-    "count": {"$sum": 1}}
-}
-
-sort = {"$sort":
-            {"_id": -1}
-        }
-
-group2 = {
-    "$group": {
-        "_id": {
-            "y": {"$year": "$"+time_key},
-            "m": {"$month": "$"+time_key},
-            "d": {"$dayOfMonth": "$"+time_key},
-            "h": {"$hour": "$"+time_key},
-            "u": "$"+unit_key
-        },
-        "count": {"$sum": 1}
-    }
-}
-
-# pipeline = [proj, group, sort]
-pipeline = [group2, sort]
-
-aggregate = col.aggregate(pipeline)
-
-for doc in aggregate:
-    pprint.pprint(doc)
-
-print('done')
+print(type(x))
